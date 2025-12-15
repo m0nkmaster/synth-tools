032b45101e06a581f35ec21d11aa9a07ec538118
Rob MacDonald <rob@robmacdonald.com>
2025-12-15 12:48:17 +0000

    Add basename to BrowserRouter for /synthtools subpath

--- src/main.tsx
+++ src/main.tsx
@@ -10,9 +10,11 @@ import VisualNodeSynth from './pages/VisualNodeSynth';
 import AIKitGenerator from './pages/AIKitGenerator';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
+const basename = import.meta.env.PROD ? '/synthtools' : '/';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Navigate to="/synthesizer" replace />} />
